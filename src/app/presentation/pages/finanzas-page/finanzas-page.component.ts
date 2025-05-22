651df648f105929407ecbaf466112eb5508f8a0f
Oscar Thielen <othielen@androvent.com>
2025-05-22 11:54:35 -0400

    cuarto commit

--- src/app/presentation/pages/finanzas-page/finanzas-page.component.ts
+++ src/app/presentation/pages/finanzas-page/finanzas-page.component.ts
@@ -1,9 +1,9 @@
-import { UpperCasePipe } from '@angular/common';
+import { CommonModule, UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-finanzas-page',
-  imports: [UpperCasePipe],
+  imports: [CommonModule, UpperCasePipe],
   templateUrl: './finanzas-page.component.html',
 })
 export class FinanzasPageComponent {
@@ -11,4 +11,11 @@ export class FinanzasPageComponent {
   firstBox = signal('Mi experiencia en el departamento de Finanzas me permitió entender su rol más allá de los números. Es un área estratégica que conecta información clave con decisiones operativas, garantiza la sostenibilidad de cada línea de negocio y ofrece una visión integral del funcionamiento de la empresa. Haber formado parte de ese proceso me ayudó a desarrollar criterio, precisión y una mirada más completa del negocio.')
 
   miRol = signal('Finance Trainee')
+
+  funcionesRealizadas: string[] = [
+    'Registro de conciliaciones bancarias',
+    'Facturación de ventas masiva (Sinapsis)',
+    'Conciliaciones con operadoras de Ekiipago',
+    'Reportes de conciliaciones'
+  ]
  }
