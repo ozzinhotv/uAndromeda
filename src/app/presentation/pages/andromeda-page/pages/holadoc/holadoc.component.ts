836b26405c23aa229863d7952f058f96698e98fd
Oscar Thielen <othielen@androvent.com>
2025-05-23 12:35:51 -0400

    sexto commit

--- src/app/presentation/pages/andromeda-page/pages/holadoc/holadoc.component.ts
+++ src/app/presentation/pages/andromeda-page/pages/holadoc/holadoc.component.ts
@@ -1,8 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-holadoc',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './holadoc.component.html',
 })
-export class HoladocComponent { }
+export class HoladocComponent { 
+
+  headerTitle = 'Holadoc';
+  headerDescription = 'Consultas médicas online desde cualquier lugar'
+
+
+  descriptionTitle = '📌 Descripción';
+  description = 'Empresa de servicios de telemedicina online con una aplicación llamada HolaDoc enfocada en realizar consultas médicas de manera virtual.'
+
+  productos = '🛠️ Productos/Servicios';
+  productosDescription = 'Servicio de consultas médicas a través de la aplicación HolaDoc.'
+
+  website = { name: 'holadoc.com', url: 'https://holadoc.com/' };
+
+  instagram = { handle: '@somosholadoc', url: 'https://instagram.com/somosholadoc' };
+
+  linkedins = [
+    { name: 'Holadoc', url: 'https://www.linkedin.com/company/holadoc' },
+    { name: 'Sinapsis', url: 'https://www.linkedin.com/company/sinapsis' }
+  ];
+
+
+
+
+
+}
