b603a86f110c35dedc33e013d638d9a6399194c9
Oscar Thielen <othielen@androvent.com>
2025-05-26 10:48:43 -0400

    septimo commit

--- src/app/presentation/pages/andromeda-page/pages/ekiipago/ekiipago.component.ts
+++ src/app/presentation/pages/andromeda-page/pages/ekiipago/ekiipago.component.ts
@@ -1,8 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ekiipago',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ekiipago.component.html',
 })
-export class EkiipagoComponent { }
+export class EkiipagoComponent {
+
+  headerTitle = 'Ekiipago';
+  headerDescription = 'Soluciones de pago tecnológicas para el comercio moderno'
+
+
+  descriptionTitle = '📌 Descripción';
+  description = 'Empresa que ofrece alternativas y soluciones de pagos innovadores y tecnológicos a distintos comercios, instituciones, páginas web, entre otros.'
+
+  productos = '🛠️ Productos/Servicios';
+  productosList: string[] = [
+    'Boton de pago',
+    'Link de pago movil',
+    'Recarga de pagos de servicios',
+  ];
+
+  website = { name: 'ekiipago.com', url: 'https://www.ekiipago.com/' };
+
+  instagram = { handle: '@ekiipago', url: 'https://instagram.com/ekiipago' };
+
+  linkedins = [
+    { name: 'Ekiipago', url: 'https://www.linkedin.com/company/ekiipago' },
+  ];
+
+ }
--- src/app/presentation/pages/andromeda-page/pages/peregrina/peregrina.component.ts
+++ src/app/presentation/pages/andromeda-page/pages/peregrina/peregrina.component.ts
@@ -1,8 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-peregrina',
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './peregrina.component.html',
 })
-export class PeregrinaComponent { }
+export class PeregrinaComponent { 
+
+  headerTitle = 'Peregrina';
+  headerDescription = 'Cervezas artesanales con innovación venezolana'
+
+
+  descriptionTitle = '📌 Descripción';
+  description = 'Fábrica de cervezas, cuyo proceso de elaboración es semi artesanal, innovando con sus recetas para obtener sabores y aromas únicos en el mercado venezolano.'
+
+  productos = '🛠️ Productos/Servicios';
+  productosList: string[] = [
+    'Vienna Lagger',
+    'Honey Light',
+    'German Pills',
+    'Munich Hells'
+
+  ];
+
+  website = { name: 'peregrinabirra.com', url: 'https://peregrinabirra.com/' };
+
+  instagram = { handle: '@peregrinabirra', url: 'https://instagram.com/peregrinabirra' };
+
+  linkedins = [
+    { name: 'Peregrina', url: 'https://www.linkedin.com/showcase/cervezaperegrina/' },
+  ];
+
+}
