f23c374a2c3eba942f1e74eff923b183cc8f3f6c
Oscar Thielen <othielen@androvent.com>
2025-06-30 16:07:28 -0400

    segunda version arreglada IV

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,9 +1,6 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Navigation, Pagination } from 'swiper/modules';
 import Swiper from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 @Component({
   selector: 'app-carousel',
